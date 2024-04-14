84e8657fa666f4a056cb1ade82d9d1ea70a63396
EmmaDjeufa <emmandjeufa@gmail.com>
2024-04-14 23:59:37 +0200

    Commit

--- frontend_movie-recommendation/src/components/common/Footer.jsx
+++ frontend_movie-recommendation/src/components/common/Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react"
-import { social } from "../data/dummydata"
 
 const Footer = () => {
   return (
     <>
       <footer className='footer'>
-        
         <p data-aos='zoom-in'>Tout droits réservés 2024</p>
       </footer>
     </>
